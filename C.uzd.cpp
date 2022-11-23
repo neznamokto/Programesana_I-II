f0e0a95bfdba80255abe2461dd63c6dff97dddfa
neznamokto <112852457+neznamokto@users.noreply.github.com>
2022-11-23 10:21:54 +0200

    Add files via upload
    Dots divu dimensiju masīvs A(n,n), kas aizpildīts ar nullēm un vieniniekiem (masīvu izveidot un ieraksīt tajā vērtības pašiem).
    Noskaidrot, cik masīva kolonnu un rindu ir aizpildītas tikai ar 1 un cik kolonnu un rindu ir aizpildītas tikai ar 0.
    Izveidot un izmantot funkciju vai funkcijas, kas vienai rindai vai kolonnai var pateikt, ka tā sastāv tikai no 1 vai 0.

---
+++ C.uzd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  int koordinates[2];
+
+  cout<<"Ieraksti x koordinates"<<endl; //rinda
+  cin>>a;
+  cout<<"Ieraksti y koordinates"<<endl; //kollona
+  cin>>b;
+  koordinates[0] = sqrt(b*b);//Y koordinâtes
+  koordinates[1] = sqrt(a*a);//X koordinâtes
+  for (int i = 0; i < koordinates[0]; i++){
+    for(int k = 0; k < koordinates[1]; k++){
+      if(i % 2 == 0){
+        if(k % 2 == 0){cout<<"0";}
+        else {cout<<"1";}
+      } else{
+        cout<<"1";
+      }
+    }
+  cout<<endl;
+  }
+}
