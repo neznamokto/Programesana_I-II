b5ec1b231dd5dd75074b3faa789ca5573dd8b2ab
neznamokto <112852457+neznamokto@users.noreply.github.com>
2022-11-27 20:59:10 +0200

    Update C.uzd.cpp

--- C.uzd.cpp
+++ C.uzd.cpp
@@ -7,18 +7,18 @@ int main(){
   int b;
   int koordinates[2];
 
-  cout<<"Ieraksti x koordinates"<<endl; //rinda
+  cout<<"Ierakstiet x koordinates"<<endl; //rinda
   cin>>a;
-  cout<<"Ieraksti y koordinates"<<endl; //kollona
+  cout<<"Ierakstiet y koordinates"<<endl; //kollona
   cin>>b;
-  koordinates[0] = sqrt(b*b);//Y koordinâtes
-  koordinates[1] = sqrt(a*a);//X koordinâtes
-  for (int i = 0; i < koordinates[0]; i++){
-    for(int k = 0; k < koordinates[1]; k++){
-      if(i % 2 == 0){
-        if(k % 2 == 0){cout<<"0";}
+  koordinates[0] = sqrt(b*b); //Y koordinÃ¢tes
+  koordinates[1] = sqrt(a*a); //X koordinÃ¢tes
+  for (int i = 0; i < koordinates[0]; i++) {
+    for(int k = 0; k < koordinates[1]; k++) {
+      if(i % 2 == 0) {
+        if(k % 2 == 0) {cout<<"0";}
         else {cout<<"1";}
-      } else{
+      } else {
         cout<<"1";
       }
     }
