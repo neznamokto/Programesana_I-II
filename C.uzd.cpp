b9261928b3c04f27b6be2c6433b7697e425e38bd
neznamokto <112852457+neznamokto@users.noreply.github.com>
2022-11-28 10:20:31 +0200

    Update C.uzd.cpp

--- C.uzd.cpp
+++ C.uzd.cpp
@@ -1,27 +1,99 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main(){
-  int a;
-  int b;
-  int koordinates[2];
+const int skr = 8, skk = 4;
 
-  cout<<"Ierakstiet x koordinates"<<endl; //rinda
-  cin>>a;
-  cout<<"Ierakstiet y koordinates"<<endl; //kollona
-  cin>>b;
-  koordinates[0] = sqrt(b*b); //Y koordinâtes
-  koordinates[1] = sqrt(a*a); //X koordinâtes
-  for (int i = 0; i < koordinates[0]; i++) {
-    for(int k = 0; k < koordinates[1]; k++) {
-      if(i % 2 == 0) {
-        if(k % 2 == 0) {cout<<"0";}
-        else {cout<<"1";}
-      } else {
-        cout<<"1";
-      }
+bool rinarNullem(int y, int masivs[skr][skk]){
+    for(int x = 0; x < skk; x++){
+        if(masivs[y][x] != 0){
+        return false;
+        }
     }
-  cout<<endl;
-  }
+    return true;
+}
+
+bool rinarViens(int y, int masivs[skr][skk]){
+    for(int x = 0; x < skk; x++){
+        if(masivs[y][x] != 1){
+        return false;
+        }
+    }
+    return true;
+}
+
+bool kolarnullem(int x, int masivs[skr][skk]){
+    for(int y = 0; y < skr; y++){
+        if(masivs[y][x] != 0){
+        return false;
+        }
+    }
+    return true;
+}
+
+bool kolarviens(int x, int masivs[skr][skk]){
+    for(int y = 0; y < skr; y++){
+        if(masivs[y][x] != 1){
+        return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+
+
+
+    int masivs[skr][skk] = {
+        {1, 0, 0, 1},
+        {1, 1, 1, 0},
+        {1, 0, 1, 1},
+        {1, 1, 1, 1},
+        {1, 1, 0, 0},
+        {1, 0, 1, 0},
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+    };
+
+    int ran = 0, rav = 0, kan = 0, kav = 0;
+
+    for (int rinda = 0; rinda < skr; rinda++){
+        if (rinarNullem(rinda, masivs) == true){
+            ran++;}
+
+        if (rinarViens(rinda, masivs) == true){
+            rav++;}
+    }
+
+    for (int kolonna = 0; kolonna < skk; kolonna++){
+        if (kolarnullem(kolonna, masivs) == true){
+            kan++;}
+
+        if(kolarviens(kolonna, masivs) == true){
+            kav++;}
+    }
+
+    if(ran > 0){
+        cout<<"rindu skaits, kas satur tikai nulles: "<<ran<<endl;
+    }else{
+        cout<<"nav rindu, kas satur tikai nulles."<<endl;
+    }
+
+    if(rav > 0){
+        cout<<"rindu skaits, kas satur tikai vieniniekus: "<<rav<<endl;
+    }else{
+        cout<<"nav rindu, kas satur tikai vieniniekus."<<endl;
+    }
+
+    if(kan > 0){
+        cout<<"kolonnu skaits, kas satur tikai nulles: "<<kan<<endl;
+    } else {
+        cout<<"nav kolonnu, kas satur tikai nulles."<<endl;
+    }
+
+    if(kav > 0){
+        cout<<"kolonnu skaits, kas satur tikai vieniniekus: "<<kav<<endl;
+    } else{
+        cout<<"nav kolonnu, kas satur tikai vieniniekus."<<endl;
+    }
+
 }
